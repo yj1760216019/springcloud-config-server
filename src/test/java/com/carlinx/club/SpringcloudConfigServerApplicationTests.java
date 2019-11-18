184175e929fb084c6cd55a620923c5221271f27e
xxxx <1760216019@qq.com>
2019-11-18 23:03:27 +0800

    springcloud配置中心

---
+++ src/test/java/com/carlinx/club/SpringcloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlinx.club;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringcloudConfigServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
